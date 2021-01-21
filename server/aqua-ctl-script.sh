e298650d60592c71bf4140af00e81664ecbbd627
Sanjay Shah <sanjay_y2k@yahoo.com>
2021-01-21 13:45:17 -0800

    Update aqua-ctl-script.sh

--- server/aqua-ctl-script.sh
+++ server/aqua-ctl-script.sh
@@ -1,1 +1,19 @@
 
+./aquactl deploy csp \
+  --platform gke \
+  --version master \
+  --admin-password **** \
+  --license **** \
+  --registry registry.aquasec.com \
+  --registry-username  <DOCKER_REGISTRY_USERNAME> \
+  --registry-password **** \
+  --aqua-username **** \
+  --aqua-password *** \
+  --enforcer-token token \
+  --server-service LoadBalancer \
+  --server-custom-envs AQUA_CLUSTER_MODE=active-active \
+  --create-pubsub-db \
+  --disable-pvcs \ 
+  --server-replicas 3 \
+  --gateway-replicas 1 \
+  --wait -y –no-spinner
